a004eed0fc7a92b5c704361f41ddd1f9cde8eeca
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-19 03:27:02 -0300

    :sparkles: Construção da Página de Cadastro do Usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
 				<Navbar />
 				<div className="min-h-[80vh]">
 					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/home"	element={<Home />}
+						<Route path="/home" element={<Home />} />
+						<Route path="/"	element={<Cadastro />}
 						/>
 					</Routes>
 				</div>
