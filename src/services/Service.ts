2dd730f9a0e7da7fbc94fbb94b25a086567a22c8
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-20 22:59:27 -0300

    :sparkles: Construção da Lógica do Componente ListaTema

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -14,3 +14,8 @@ export const login = async (url: string, dados: Object, setDados: Function) => {
     setDados(resposta.data)
 }
 
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
