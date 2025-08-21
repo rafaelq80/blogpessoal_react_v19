4c5da7ff099debe60b34d8f69754defa42751344
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-21 19:10:11 -0300

    :sparkles: Construção da Lógica do Componente FormTema

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal.onrender.com'
+    //baseURL: 'https://blogpessoal.onrender.com'
+    baseURL: 'http://localhost:8080'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
@@ -19,3 +20,12 @@ export const buscar = async (url: string, setDados: Function, header: Object) =>
     setDados(resposta.data)
 }
 
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
