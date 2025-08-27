901b47c6af9525e95c1bdddc540595f2dc78cfd5
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-27 09:25:40 -0300

    :sparkles: Construção do Componente DeletarPostagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagem'
+import FormPostagem from './components/postagem/formpostagem/FormPostagem'
 import ListaPostagens from './components/postagem/listapostagens/ListaPostagens'
 import DeletarTema from './components/tema/deletartema/DeletarTema'
 import FormTema from './components/tema/formtema/FormTema'
@@ -9,7 +11,6 @@ import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import FormPostagem from './components/postagem/formpostagem/FormPostagem'
 
 function App() {
 	return (
@@ -29,6 +30,7 @@ function App() {
 							<Route path="/postagens" element={<ListaPostagens />} />
 							<Route path="/cadastrarpostagem" element={<FormPostagem />} />
 							<Route path="/editarpostagem/:id" element={<FormPostagem />} />
+							<Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
 						</Routes>
 					</div>
 					<Footer />
