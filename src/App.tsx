1832543454aac6b918f1b7cc8b0a63f6687a33e1
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-27 10:30:24 -0300

    :sparkles: Cosntrução da Página de Perfil do Usuário (Estilização + Lógica)

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
 	return (
@@ -31,6 +32,7 @@ function App() {
 							<Route path="/cadastrarpostagem" element={<FormPostagem />} />
 							<Route path="/editarpostagem/:id" element={<FormPostagem />} />
 							<Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+							<Route path="/perfil" element={<Perfil />} />
 						</Routes>
 					</div>
 					<Footer />
