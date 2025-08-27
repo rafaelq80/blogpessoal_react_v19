694a11a9b4aa434cf8a4a2fc51b468ecba619553
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-27 11:49:01 -0300

    :lipstick: Adição dos Alertas Personalizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagem'
@@ -13,10 +14,13 @@ import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Perfil from './pages/perfil/Perfil'
 
+import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
 	return (
 		<>
 			<AuthProvider>
+				<ToastContainer />
 				<BrowserRouter>
 					<Navbar />
 					<div className="min-h-[80vh]">
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal.onrender.com'
+    //baseURL: 'https://blogpessoal.onrender.com'
+    baseURL: 'http://localhost:8080'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
